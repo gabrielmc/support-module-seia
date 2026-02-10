9bb8a1e8ac5f84a174d8b65991df2bbaec0cace2
Gabriel Muniz <munhosga@gmail.com>
2026-02-10 11:53:53 -0300

    Add processos_routes to FastAPI application
    Add processos_routes to FastAPI application

--- module-seia/app/main.py
+++ module-seia/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.routes.requerimentos_routes import router as requerimentos_router
 from app.routes.consultas_routes import router as consultas_router
 from app.routes.relatorios_routes import router as relatorios_router
+from app.routes.processos_routes import router as processos_router
 
 app = FastAPI(
     title="Module-SEIA",
@@ -12,3 +13,4 @@ app = FastAPI(
 app.include_router(requerimentos_router)
 app.include_router(consultas_router)
 app.include_router(relatorios_router)
+app.include_router(processos_router)
