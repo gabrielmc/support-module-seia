9b52353e8e2d23e87240a82b02cf0b66663a1634
Gabriel Muniz <munhosga@gmail.com>
2026-02-11 16:23:37 -0300

    Implement logical deletion of requerimento
    Added a method to logically delete a request based on provided details.

--- module-seia/app/services/processo_service.py
+++ module-seia/app/services/processo_service.py
@@ -1,16 +1,47 @@
-from typing import List, Optional
+from typing import Optional
 from app.repositories.processo_repository import ProcessoRepository
 
 
 class ProcessoService:
 
-    """
-    Ajustando status da tabela de controle_tramitacao
-    """
     @staticmethod
     def removendo_status_anterior(tramitacao: Optional[str] = None):
         try:
             return ProcessoRepository().removendo_status_anterior(tramitacao=tramitacao)
         except Exception as e:
             print(f"Erro ao remover status anterior: {e}")
+            raise e
+    
+    @staticmethod 
+    def excluir_requerimento_logicamente(requerente: str, cpf_cnpj: str, imovel: str):
+        try:
+            resultado = ProcessoRepository().buscar_requerimento_por_dados(
+                requerente=requerente,
+                cpf_cnpj=cpf_cnpj,
+                imovel=imovel
+            )
+            if not resultado:
+                return {
+                    "sucesso": False,
+                    "mensagem": "Nenhum requerimento encontrado"
+                }
+
+            ide_requerimento = resultado["ide_requerimento"]
+            ide_requerimento_imovel = resultado["ide_requerimento_imovel"]
+            if not ide_requerimento or not ide_requerimento_imovel:
+                return {
+                    "sucesso": False,
+                    "mensagem": "IDs inválidos para exclusão."
+                }
+            # Executa updates
+            ProcessoRepository().excluir_requerimento(ide_requerimento)
+            ProcessoRepository().excluir_requerimento_imovel(ide_requerimento_imovel)
+            return {
+                "sucesso": True,
+                "ide_requerimento": ide_requerimento,
+                "ide_requerimento_imovel": ide_requerimento_imovel,
+                "mensagem": "Requerimento excluído logicamente com sucesso"
+            }
+        except Exception as e:
+            print(f"Erro ao excluir requerimento logicamente: {e}")
             raise e
