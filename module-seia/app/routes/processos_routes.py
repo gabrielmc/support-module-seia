4c1856f406ad130f34f4ea7d74870cdb99c0c84a
Gabriel Muniz <munhosga@gmail.com>
2026-02-11 16:24:12 -0300

    Add endpoint to exclude duplicate requests
    Added a new endpoint to exclude duplicate requests logically.

--- module-seia/app/routes/processos_routes.py
+++ module-seia/app/routes/processos_routes.py
@@ -1,11 +1,13 @@
 # app/routes/processos_routes.py
 
+import re
 from fastapi import APIRouter
 from app.services.processo_service import ProcessoService
+from app.models.schemas.general_schemas import *
 
 router = APIRouter(
     prefix="/processos",
-    tags=["PROCESSO"]
+    tags=["PROCESSOS & CEFIR"]
 )
 
 @router.post("/processo/{identificador}")
@@ -16,3 +18,19 @@ async def alterar_status_controle_tramitacao(controle_tramitacao: str):
     except Exception as e:
         print(f"Erro ao alterar status controle tramitacao: {e}")
         return {"error": str(e)}
+
+
+@router.post("/cefir/excluir-duplicado")
+async def excluir_requerimento_duplicado(payload: ExcluirRequerimentoLogico):
+    try:
+        cnpj_cpf_limpo = re.sub(r"\D", "", payload.cpf_cnpj)
+        service = ProcessoService().excluir_requerimento_logicamente(
+            requerente=payload.requerente,
+            cpf_cnpj=cnpj_cpf_limpo,
+            imovel=payload.imovel
+        )
+        return service
+    except Exception as e:
+        print(f"Erro ao excluir requerimento duplicado: {e}")
+        return {"error": str(e)}
+        #raise HTTPException(status_code=500, detail=str(e))
