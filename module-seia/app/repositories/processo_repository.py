72c628b027fc7dfde8060244ea0c2485e94166da
Gabriel Muniz <munhosga@gmail.com>
2026-02-11 16:24:56 -0300

    Add methods to search and delete requerimento

--- module-seia/app/repositories/processo_repository.py
+++ module-seia/app/repositories/processo_repository.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import Optional
 from app.core.database import get_db_connection
 
 
@@ -7,6 +7,7 @@ class ProcessoRepository:
     desenvolvimento = "DSV"
     homologacao = "HML"
 
+    #Ajustando status da tabela de controle_tramitacao
     def removendo_status_anterior(self, tramitacao: Optional[str] = None):
         query = """
             UPDATE controle_tramitacao ct
@@ -29,3 +30,70 @@ class ProcessoRepository:
                 "tramitacao": row[0],
                 "status": row[1]
             }
+    
+    def buscar_requerimento_por_dados(self, requerente: str, cpf_cnpj: str, imovel: str):
+        query = """
+            SELECT R.IDE_REQUERIMENTO_IMOVEL, R.IDE_REQUERIMENTO
+                FROM REQUERIMENTO_IMOVEL R
+            WHERE R.IDE_IMOVEL IN (
+                SELECT IR.IDE_IMOVEL_RURAL
+                    FROM IMOVEL_RURAL IR
+                WHERE IR.IDE_IMOVEL_RURAL IN (
+                    SELECT T.IDE_IMOVEL
+                        FROM PESSOA_IMOVEL T
+                    WHERE T.IDE_PESSOA = (
+                        SELECT PF.IDE_PESSOA_FISICA
+                            FROM PESSOA_FISICA PF
+                        WHERE PF.NOM_PESSOA ILIKE %(requerente)s
+                        AND PF.NUM_CPF ILIKE %(cpf_cnpj)s
+                    )
+                )
+                AND IR.DES_DENOMINACAO ILIKE %(imovel)s
+            )
+            LIMIT 1;
+        """
+        with get_db_connection(self.desenvolvimento) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(
+                    query,
+                    {
+                        "requerente": f"%{requerente}%",
+                        "cpf_cnpj": f"%{cpf_cnpj}%",
+                        "imovel": f"%{imovel}%"
+                    }
+                )
+                row = cursor.fetchone()
+        if not row:
+            return None
+        return {
+            "ide_requerimento_imovel": row[0],
+            "ide_requerimento": row[1]
+        }
+    
+    # Update requerimento
+    def excluir_requerimento(self, ide_requerimento: int):
+        query = """
+            UPDATE requerimento
+            SET ind_excluido = TRUE,
+                dtc_exclusao = now()
+            WHERE ide_requerimento = %(ide_requerimento)s;
+        """
+        with get_db_connection(self.desenvolvimento) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(query, {
+                    "ide_requerimento": ide_requerimento
+                })
+
+    # Update requerimento_imovel
+    def excluir_requerimento_imovel(self, ide_requerimento_imovel: int):
+        query = """
+            UPDATE requerimento_imovel
+            SET ind_excluido = TRUE,
+                dtc_exclusao = now()
+            WHERE ide_requerimento_imovel = %(ide_requerimento_imovel)s;
+        """
+        with get_db_connection(self.desenvolvimento) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(query, {
+                    "ide_requerimento_imovel": ide_requerimento_imovel
+                })
