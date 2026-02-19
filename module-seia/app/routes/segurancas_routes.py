3417b9890563e932be196fea60cb18cc7baba376
Gabriel Muniz <munhosga@gmail.com>
2026-02-19 13:27:56 -0300

    Add logging and new routes for email handling

--- module-seia/app/routes/segurancas_routes.py
+++ module-seia/app/routes/segurancas_routes.py
@@ -1,7 +1,12 @@
 # app/routes/segurancas_routes.py
 
+import logging
 from fastapi import APIRouter
 from app.services.seguranca_service import SegurancaService
+from app.models.schemas.general_schemas import ListaUsuariosEmail, ListaUsuariosCPF
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("seguranca")
 
 router = APIRouter(
     prefix="/seguranca",
@@ -16,3 +21,26 @@ async def up_perfil_administrativo(nome_usuario: str):
     except Exception as e:
         print(f"Erro ao atualizar perfil administrativo: {e}")
         return {"error": str(e)}
+
+@router.post("/inclui-email")
+async def incluir_email_usuario(payload: ListaUsuariosEmail):
+    try:
+        service = SegurancaService().incluir_email_usuario(payload.usuarios)
+        return service
+    except Exception as e:
+        print(f"Erro ao incluir email do usuário: {e}")
+        return {"sucesso": False, "error": str(e)}
+
+@router.post("/gerar-script-email-cpf")
+async def gerar_script(payload: ListaUsuariosCPF):
+    #logger.info(f"REQUEST - POST - /gerar-script-email-cpf")
+    try:
+        file_path = SegurancaService().gerar_script_email_por_cpf(payload.usuarios)
+        return {
+            "sucesso": True,
+            "arquivo_gerado": file_path
+        }
+    except Exception as e:
+        logger.warning(f"EXCEPTION - POST - /gerar-script-email-cpf erro : {e}")
+        print(f"Erro ao gerar script de email por CPF: {e}")
+        return {"sucesso": False, "error": str(e)}
