227c23f9f1e90d7bc5a7aa11aa3f8a480db3b0d3
Gabriel Muniz <munhosga@gmail.com>
2026-03-11 13:56:33 -0300

    Update segurancas_routes.py

--- module-seia/app/routes/segurancas_routes.py
+++ module-seia/app/routes/segurancas_routes.py
@@ -19,7 +19,6 @@ async def up_perfil_administrativo(payload: UsuarioPerfil):
         return service
     except Exception as e:
         logger.warning(f"EXCEPTION - POST - /alterar-perfil/[params] : {e}")
-        print(f"Erro ao atualizar perfil administrativo: {e}")
         return {"error": str(e)}
 
 @router.post("/inclui-email")
@@ -30,7 +29,6 @@ async def incluir_email_usuario(payload: ListaUsuariosEmail):
         return service
     except Exception as e:
         logger.warning(f"EXCEPTION - POST - /inclui-email erro : {e}")
-        print(f"Erro ao incluir email do usuário: {e}")
         return {"sucesso": False, "error": str(e)}
 
 @router.post("/gerar-script-email-cpf")
@@ -44,19 +42,4 @@ async def gerar_script(payload: ListaUsuariosCPF):
         }
     except Exception as e:
         logger.warning(f"EXCEPTION - POST - /gerar-script-email-cpf erro : {e}")
-        print(f"Erro ao gerar script de email por CPF: {e}")
-        return {"sucesso": False, "error": str(e)}
-
-@router.post("/executar-scripts-zip")
-async def executar_scripts_zip(file: UploadFile = File(...)):
-    logger.info(f"POST /gerar-script-email-cpf")
-    try:
-        if not file.filename.endswith(".zip"):
-            raise HTTPException(status_code=400, detail="Arquivo deve ser .zip")
-        print(f"Recebido arquivo: {file.filename}")
-        resultado = await SegurancaService().processar_zip_e_executar(file)
-        return resultado
-    except Exception as e:
-        logger.warning(f"EXCEPTION - POST - /executar-scripts-zip erro : {e}")
-        print(f"Erro ao executar scripts zip: {e}")
         return {"sucesso": False, "error": str(e)}
